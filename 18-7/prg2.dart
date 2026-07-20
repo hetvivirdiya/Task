25086e981e4a2c528fb937441e5f4d4dec609704
Hetvi Virdiya <virdiyahetvi@gmail.com>
2026-07-20 12:20:17 +0530

    Create prg2.dart

---
+++ 18-7/prg2.dart
@@ -0,0 +1,17 @@
+2. Write a program to make addition, Subtraction, Multiplication and Division of
+Two Numbers.
+
+  void main()
+{
+  int a=6;
+  int b=4;
+
+  var add=a+b;
+  print(add);
+  var sub=a-b;
+  print(sub);
+  var mul=a*b;
+  print(mul);
+  var div=a/b;
+  print(div);
+}
