aaec4046ade49f4c67d1b5b352fe5d8fbcca6184
Hetvi Virdiya <virdiyahetvi@gmail.com>
2026-07-20 12:21:39 +0530

    Create prg3.dart

---
+++ 18-7/prg3.dart
@@ -0,0 +1,10 @@
+3. Write a program to make a square and cube of number.
+
+  void main()
+{
+  var follower=1500;
+  var following=500;
+  var difference=follower-following;
+
+  print("difference: $difference");
+}
