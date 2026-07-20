5d69418687840d4efbe73110a3e50f3d61c9fef0
Hetvi Virdiya <virdiyahetvi@gmail.com>
2026-07-20 12:19:38 +0530

    Create prg1.dart

---
+++ 18-7/prg1.dart
@@ -0,0 +1,14 @@
+1. Display This Information using print
+a. Your Name
+b. Your Birth date
+c. Your Age
+d. Your Address
+
+
+  void main()
+{
+  print("My name is Hetvi");
+  print("My birth date is 30-05-2006");
+  print("My age is 21");
+  print("My address is Gondal");
+}
